cab62143b0a730cc51b3309b9a8f6414da91d89e
AlexandrSawrasov <caiiiuk@mail.ru>
2022-10-14 01:29:14 +0300

    Task2 on

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -4,3 +4,24 @@
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
 
+Console.Write("Введите a: ");
+var A = Console.ReadLine();
+Console.Write("Введите b: ");
+var B = Console.ReadLine();
+if (int.TryParse(A, out int a) && int.TryParse(B, out int b))
+{
+    int max = 0;
+    if (a > b)
+    {
+        max = a;
+    }
+    else
+    {
+        max = b;
+    }
+    Console.WriteLine("a = " + a + "; b = " + b + " -> max = " + max);
+}
+else
+{
+    Console.WriteLine("Входные данные не являются числами!");
+}
