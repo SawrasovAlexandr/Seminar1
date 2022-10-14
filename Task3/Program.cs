9af89b5549013edd9ee8eb1196b694536a5de853
AlexandrSawrasov <caiiiuk@mail.ru>
2022-10-14 16:05:44 +0300

    Task3 On

---
+++ Task3/Program.cs
@@ -0,0 +1,42 @@
+﻿// 3. Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+
+//     3 -> Среда 
+//     5 -> Пятница
+
+Console.WriteLine("Введите номер дня недели: ");
+int x = Convert.ToInt16(Console.ReadLine());
+if (x <= 7 && x >= 1)
+{
+    if (x == 1)
+    {
+        Console.WriteLine("1 -> Понедельник");
+    }
+    if (x == 2)
+    {
+        Console.WriteLine("2 -> Вторник");
+    }
+    if (x == 3)
+    {
+        Console.WriteLine("3 -> Среда");
+    }
+    if (x == 4)
+    {
+        Console.WriteLine("4 -> Четверг");
+    }
+    if (x == 5)
+    {
+        Console.WriteLine("5 -> Пятница");
+    }
+    if (x == 6)
+    {
+        Console.WriteLine("6 -> Суббота");
+    }
+    if (x == 7)
+    {
+        Console.WriteLine("7 -> Воскресенье");
+    }
+}
+else
+{
+    Console.WriteLine("Введенное число не принадлежит заданному диапазону");
+}
